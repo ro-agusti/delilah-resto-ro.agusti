3489623d0c1f1ad3eaf0d64aa52d1c1ef45b8e45
ro <rochoagusti@gmail.com>
2021-08-15 11:50:39 -0300

    readme finished and data base imported

---
+++ README.md
@@ -0,0 +1,63 @@
+# SPRINT 3 = Backend - "Delilah Restó", App de pedidos de comida
+
+## Recursos y tecnologías utilizadas
+
+- Node.js
+- Nodemon
+- Express
+- Jason Web Token para autenticación via Token
+- Helmet
+- MySQL2
+- Sequelize
+- Cors
+- Dotenv para manejo de endpoints y testing
+- Swagger para documentación de API
+
+El objetivo del trabajo es generar el backend de una app de pedidos de comida, creando la bases de datos relacionales, endpoints funcionales, middlewares necesarios y documentación.
+
+## Documentación de la API
+
+//
+
+
+## Instalación e inicializacion del proyecto
+
+### 1 - Clonar proyecto
+
+Clonar el repositorio desde el [siguiente link](https://github.com/ro-agusti/delilah-resto-ro.agusti).
+
+### 2 - Instalar de dependencias
+
+* Asegurarse de estar parado en la carpeta clonada desde la terminal y colocar en ella
+
+```
+npm install
+```
+
+### 3 - Importar base de datos
+
+- Abrir XAMPP y asegurarse que el puerto sobre el cual se está ejecutando es el `3306`, sino puede modificar el archivo `.env` que se encuentra en el indice del proyecto, ahi podra modificar los datos de acuerdo a su dispositivo y sus datos:
+    - PORT_DATA_BASE=`3306`
+    - NAME_DATA_BASE= `sprint3_delila-resto`
+    - USERNAME_DATA_BASE= `root`
+    - PASSWORD_BATA_BASE= ` `
+- Inicializar los servicios de Apache y MySQL
+- Abrir el panel de control del servicio MySQL
+- Generar una nueva base de datos llamada `sprint3_delila-resto` desde el panel de control, si desea colocarle otro nombre lo podra hacer pero siempre modificando tambien el archivo `.env` como aclare anteriormente
+- Importar el archivo en `/database/sql/sprint3_delila-resto.sql` y dentro de PHP myAdmin, podra importarlo.
+
+### 4 - Iniciar el servidor
+
+* Asegurarse de estar parado en la carpeta clonada desde la terminal y colocar en ella
+
+`node app.js`
+
+O desde `nodemon app.js` si es que lo tiene instalado de manera global en su dispositivo.
+
+### 5 - Listo para empezar!
+Testear los endpoints provistos desde el archivo `request.rest` colocada en el indice de la carpeta clonada o desde `Postman` para poder hacer uso de la API y base de datos generadas
+
+
+
+ -*-*-*-*-*-*-*--*-*-*
+[Colección de Postman](https://documenter.getpostman.com/view/10237996/SzYdSbvb)
---
+++ database/sql/sprint3_delila-resto.sql
@@ -0,0 +1,182 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 15, 2021 at 03:40 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sprint3_delila-resto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `ID_order` int(11) NOT NULL,
+  `orders_ID` int(11) NOT NULL,
+  `product_ID` int(11) NOT NULL,
+  `amount` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `ID_orders` int(11) NOT NULL,
+  `state` enum('NUEVO','CONFIRMADO','PREPARANDO','ENVIANDO','ENTREGADO') NOT NULL DEFAULT 'NUEVO',
+  `time` datetime NOT NULL DEFAULT current_timestamp(),
+  `payment_type` enum('TARJETA','TRANSFERENCIA','EFECTIVO') NOT NULL,
+  `user_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ID_product` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `price` int(100) NOT NULL,
+  `image` tinyblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ID_product`, `product_name`, `price`, `image`) VALUES
+(4, 'pizza', 550, ''),
+(8, 'LOMITO', 700, ''),
+(9, 'hamburguesa', 350, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `ID_user` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `name & surname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telephone` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `role` enum('USER','ADMIN') NOT NULL DEFAULT 'USER'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`ID_user`, `username`, `name & surname`, `email`, `telephone`, `address`, `password`, `role`) VALUES
+(2, 'ADMINISTRADOR', 'ADMINISTRADOR5', 'user1@hotmail.com', '21354548', 'san martin 452', '12345Us##', 'ADMIN'),
+(3, 'user1', 'user modificado5', 'user1@hotmail.com', '21354548', 'san martin 452', '12345Us#', 'USER'),
+(25, 'user2', 'user1', 'user1@hotmail.com', '21354548', 'san martin 452', '12345Us#', 'USER'),
+(26, 'user4', 'user1', 'user1@hotmail.com', '21354548', 'san martin 452', '12345Us#', 'USER'),
+(27, 'user5', 'user1', 'user1@hotmail.com', '21354548', 'san martin 452', '12345Us#', 'USER'),
+(28, 'user7', 'user1', 'user1@hotmail.com', '21354548', 'san martin 452', '12345Us#', 'USER'),
+(29, 'user8', 'user1', 'user1@hotmail.com', '21354548', 'san martin 452', '12345Us#', 'USER');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`ID_order`),
+  ADD KEY `orders_ID` (`orders_ID`),
+  ADD KEY `product_ID` (`product_ID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`ID_orders`),
+  ADD KEY `user_ID` (`user_ID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ID_product`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `ID_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `ID_orders` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ID_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`orders_ID`) REFERENCES `orders` (`ID_orders`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_ibfk_2` FOREIGN KEY (`product_ID`) REFERENCES `products` (`ID_product`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_ID`) REFERENCES `users` (`ID_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
